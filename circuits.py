eba61fa746fe019a069a99334be63cd312529f78
LAPTOP-P7CUSO1H\ishaa <ishaan.roy@sleepiz.com>
2020-10-07 21:09:25 +0530

    Added functionality for random parameters

--- circuits.py
+++ circuits.py
@@ -1,32 +1,35 @@
 import cirq as cq
 import numpy as np
+import sympy as sp
 
-# def main():
-#     qubit = cq.LineQubit.range(4)
-#     a = ((np.random.rand(4))*4)
+def main():
     
-#     circuit = cq.Circuit()
-#     for theta, q in zip(*[a, qubit]):
-#         circuit.append(cq.rx(theta).on(q))
+    qubit = cq.LineQubit.range(4)
+    a, b, c, d = sp.symbols('a b c d')
+    circuit = cq.Circuit()
+    circuit.append(cq.rx(a).on(qubit[0]))
+    circuit.append(cq.rx(b).on(qubit[1]))
+    circuit.append(cq.rx(c).on(qubit[2]))
+    circuit.append(cq.rx(d).on(qubit[3]))
 
-#     for i, q in enumerate(qubit):
-#         if i == 0:
-#             circuit.append(cq.CZ(q, q+1), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
-#             circuit.append(cq.CZ(q, q+2), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
-#             circuit.append(cq.CZ(q, q+3), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
-#         elif i == 1:
-#             circuit.append(cq.CZ(q, q+1), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
-#             circuit.append(cq.CZ(q, q+2), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
-#         elif i == 2:
-#             circuit.append(cq.CZ(q, q+1), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
+    for i, q in enumerate(qubit):
+        if i == 0:
+            circuit.append(cq.CZ(q, q+1), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
+            circuit.append(cq.CZ(q, q+2), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
+            circuit.append(cq.CZ(q, q+3), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
+        elif i == 1:
+            circuit.append(cq.CZ(q, q+1), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
+            circuit.append(cq.CZ(q, q+2), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
+        elif i == 2:
+            circuit.append(cq.CZ(q, q+1), strategy=cq.InsertStrategy.NEW_THEN_INLINE)
             
 
-#         else:
-#             break
+        else:
+            break
         
 
-#     print(circuit)
-#     # sim_result = simulation(circuit, 20)
+    print(circuit)
+    # sim_result = simulation(circuit, 20)
     
 
 # def simulation(circuit, repetitions):
@@ -35,12 +38,10 @@ import numpy as np
 #     return result
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
+
+# theta = sp.symbols('theta'*4)
+# print(theta)
 
 
-
-
-a = np.random.rand(4, 4)
-print(a)
-print(a[:, 0])
--- state_min.py
+++ state_min.py
@@ -1,26 +1,33 @@
 import cirq as cq
 import numpy as np
+import sympy as sp
+import random
 
 class LayerCircuit:
 
     def __init__(self, layers, qubit_count):
-        self.layers = layers
+        self.layers = layers/2
         self.qubit_count = qubit_count
         self.qubit_in = cq.LineQubit.range(qubit_count)
-        self.params = (np.random.rand(qubit_count))*4
-        self.theta = (np.random.rand(qubit_count, 2*layers))*4
+    
 
-    def layer_odd(self, params):
+    def layer_odd(self):
         circuit = cq.Circuit()
-        for theta, q in zip(*[params, self.qubit_in]):
-            circuit.append(cq.rx(theta).on(q))
+        a, b, c, d = sp.symbols('a b c d')
+        circuit.append(cq.rx(a).on(self.qubit_in[0]))
+        circuit.append(cq.rx(b).on(self.qubit_in[1]))
+        circuit.append(cq.rx(c).on(self.qubit_in[2]))
+        circuit.append(cq.rx(d).on(self.qubit_in[3]))
       
         return circuit
     
-    def layer_even(self, params):
+    def layer_even(self):
         circuit = cq.Circuit()
-        for theta, q in zip(*[params, self.qubit_in]):
-            circuit.append(cq.rx(theta).on(q))
+        a, b, c, d = sp.symbols('a b c d')
+        circuit.append(cq.rx(a).on(self.qubit_in[0]))
+        circuit.append(cq.rx(b).on(self.qubit_in[1]))
+        circuit.append(cq.rx(c).on(self.qubit_in[2]))
+        circuit.append(cq.rx(d).on(self.qubit_in[3]))
         
         for i, q in enumerate(self.qubit_in):
 
@@ -41,19 +48,43 @@ class LayerCircuit:
 
     def block_circuit(self):
         circuit = cq.Circuit()
-        index = 0
-        for _ in range((self.theta.shape[1])):
-            if index > self.layers:
-                break
-            circuit_odd = self.layer_odd(params=self.theta[:, index])
-            circuit_evn = self.layer_even(params=self.theta[:, index+1])
-            circuit.append([circuit_odd, circuit_evn], strategy=cq.InsertStrategy.NEW_THEN_INLINE)
-            index += 2
+        a, b, c, d = sp.symbols('a b c d')
+        for _ in range(int(2*self.layers)):
+            parameters = {
+                a: random.uniform(0, 1),
+                b: random.uniform(0, 1),
+                c: random.uniform(0, 1),
+                d: random.uniform(0, 1)
+            }
+            resolver = cq.ParamResolver(parameters)
+            odd_circuit = self.layer_odd()
+            evn_circuit = self.layer_even()
+            circuit.append([odd_circuit, evn_circuit], strategy=cq.InsertStrategy.NEW_THEN_INLINE)
+            trial_results = cq.Simulator().simulate(circuit, resolver)
+            state_vec = trial_results._final_simulator_state
+           
             
-        return circuit
+        return state_vec.state_vector
 
+    def simulate_layer_circuit(self, circuit):
+        simulator  = cq.Simulator()
+        trial_results = simulator.simulate(circuit)
+        state_vec = trial_results._final_simulator_state
+        
+        return state_vec.state_vector
+
+    def find_distance(self, state_vector):
+        dim = self.qubit_count**2
+        random_init_vector = np.random.rand(dim)
+        return np.linalg.norm(state_vector-random_init_vector)
+
+    
+
+  
     
 
 layer_obj = LayerCircuit(2, 4)
 block_circuit_obj = layer_obj.block_circuit()
+# final_state = layer_obj.simulate_layer_circuit(block_circuit_obj)
+# distance = layer_obj.find_distance(final_state)
 print(block_circuit_obj)
